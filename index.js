27c9700cd759115f966ef92e2ebeb957f66a1585
industrious-generator-6081 <graywolf.niehaus@live.com>
2020-02-07 18:13:30 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,24 @@
+function add(number){
+  return number++
+}function subtract(number){
+  return number--
+}function multiple(number){
+  return number*=2
+}function divide(number){
+  return number/=2
+}
+
+function add(a,b){
+  return a+b
+}function subtract(a,b){
+  return a-b
+}function multiple(a,b){
+  return a*b;
+}function divide(a,b){
+  return a/b
+}
+
+function makeInt(n){
+  var x = parseInt('10', n)
+  return x
+}
